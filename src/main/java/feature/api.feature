cbbb0149617ef201ce9d346c0d0c02aeddb81bb4
DaffaPintu <daffa@pintu.co.id>
2024-07-22 07:29:58 +0700

    FIX

--- src/main/java/feature/api.feature
+++
@@ -1,3 +0,0 @@
-Feature: Test Automation Rest Api
-  Scenario: Test get list data normal
-    Given prepare url valid for get list data
--- src/main/resources/Apistep.java
+++
@@ -1,5 +0,0 @@
-public class Apistep {
-    @io.cucumber.java.en.Given("^prepare url valid for get list data$")
-    public void prepareUrlValidForGetListData() {
-    }
-}
